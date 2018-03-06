19428ca493fd85139061d09a9db90cff51f5d015
Isabella Melendez <Isabella Melendez@143.44.77.48>
2018-03-06 19:55:10 -0400

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/gameclient/FXMLLobbyController.java
+++ src/gameclient/FXMLLobbyController.java
@@ -27,29 +27,41 @@ public class FXMLLobbyController implements Initializable, GameConstants  {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        gateway = new GameGateway();
+        
         new Thread(() -> {
-        try {
-        // Get notification from the server
-        int player = gateway.getPlayerNumber();
+            try {
+                // Get notification from the server
+                int player = gateway.getPlayerNumber();
+                System.out.println("help"+player);
+                // Am I player 1 or 2?
+                if (player == PLAYER1) {
+                    //myName = "PLAYER 1";
+                    //otherName = "PLAYER 2";
+                    Platform.runLater(() -> {
+                        lblTitle.setText("PLAYER 1");
+                        lblStatus.setText("Waiting for player 2 to join");
+                    });
+                    // Receive startup notification from the server
+                    gateway.getPlayerNumber(); // Whatever read is ignored WHAT????????
+                    // The other player has joined
+                    Platform.runLater(() ->
+                            lblStatus.setText("Player 2 has joined"));
+                }
+                else if (player == PLAYER2) {
 
-        // Am I player 1 or 2?
-        if (player == PLAYER1) {
-        myName = "PLAYER 1";
-        otherName = "PLAYER 2";
-        Platform.runLater(() -> {
-        lblTitle.setText("PLAYER 1");
-        lblStatus.setText("Waiting for player 2 to join");
-        });
-        // Receive startup notification from the server
-        gateway.getPlayerNumber(); // Whatever read is ignored
-        // The other player has joined
-        Platform.runLater(() ->
-        lblStatus.setText("Player 2 has joined"));
-        }
-       }
-        catch (Exception ex) {
-        ex.printStackTrace();}
-    }).start();
+                    Platform.runLater(() -> {
+                    lblTitle.setText("PLAYER 2");
+                    lblStatus.setText("Start Game");
+                     });
+                }
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }).start();
+        
+        //NEW STAGE????
         
         new Thread (() ->{
         try{
